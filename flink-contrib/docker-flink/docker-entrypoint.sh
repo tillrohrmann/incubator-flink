10fd3f9e4ffb8a11d679ea35f210d0d36e2f2949
Till Rohrmann <trohrmann@apache.org>
2018-06-07 09:13:38 +0200

    [hotfix] Fix blob and qs port in docker-entrypoint.sh

--- flink-contrib/docker-flink/docker-entrypoint.sh
+++ flink-contrib/docker-flink/docker-entrypoint.sh
@@ -28,6 +28,8 @@ if [ "$1" == "--help" -o "$1" == "-h" ]; then
 elif [ "$1" == "jobmanager" ]; then
     echo "Starting Job Manager"
     sed -i -e "s/jobmanager.rpc.address: localhost/jobmanager.rpc.address: ${JOB_MANAGER_RPC_ADDRESS}/g" $FLINK_HOME/conf/flink-conf.yaml
+    echo "blob.server.port: 6124" >> "$FLINK_HOME/conf/flink-conf.yaml"
+    echo "query.server.port: 6125" >> "$FLINK_HOME/conf/flink-conf.yaml"
 
     echo "config file: " && grep '^[^\n#]' $FLINK_HOME/conf/flink-conf.yaml
     exec $FLINK_HOME/bin/jobmanager.sh start-foreground cluster
@@ -35,6 +37,8 @@ elif [ "$1" == "taskmanager" ]; then
 
     sed -i -e "s/jobmanager.rpc.address: localhost/jobmanager.rpc.address: ${JOB_MANAGER_RPC_ADDRESS}/g" $FLINK_HOME/conf/flink-conf.yaml
     sed -i -e "s/taskmanager.numberOfTaskSlots: 1/taskmanager.numberOfTaskSlots: $(grep -c ^processor /proc/cpuinfo)/g" $FLINK_HOME/conf/flink-conf.yaml
+    echo "blob.server.port: 6124" >> "$FLINK_HOME/conf/flink-conf.yaml"
+    echo "query.server.port: 6125" >> "$FLINK_HOME/conf/flink-conf.yaml"
 
     echo "Starting Task Manager"
     echo "config file: " && grep '^[^\n#]' $FLINK_HOME/conf/flink-conf.yaml
