shaolp/flink

[FLINK-11843] Add proper per-job mode support

flink-runtime/src/main/java/org/apache/flink/runtime/dispatcher/runner/DispatcherLeaderProcessFactoryFactory.java
@@ -1,0 +1,39 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.runtime.dispatcher.runner;
+
+import org.apache.flink.runtime.dispatcher.PartialDispatcherFactoryServices;
+import org.apache.flink.runtime.jobmanager.JobGraphStoreFactory;
+import org.apache.flink.runtime.rpc.FatalErrorHandler;
+import org.apache.flink.runtime.rpc.RpcService;
+
+import java.util.concurrent.Executor;
+
+/**
+ * Factory for {@link DispatcherLeaderProcessFactory}.
+ */
+public interface DispatcherLeaderProcessFactoryFactory {
+
+	DispatcherLeaderProcessFactory createFactory(
+		JobGraphStoreFactory jobGraphStoreFactory,
+		Executor ioExecutor,
+		RpcService rpcService,
+		PartialDispatcherFactoryServices dispatcherFactoryServices,
+		FatalErrorHandler fatalErrorHandler);
+}

flink-runtime/src/main/java/org/apache/flink/runtime/dispatcher/runner/JobDispatcherLeaderProcessFactoryFactory.java
@@ -1,0 +1,74 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.runtime.dispatcher.runner;
+
+import org.apache.flink.runtime.dispatcher.JobDispatcherFactory;
+import org.apache.flink.runtime.dispatcher.PartialDispatcherFactoryServices;
+import org.apache.flink.runtime.entrypoint.component.JobGraphRetriever;
+import org.apache.flink.runtime.jobgraph.JobGraph;
+import org.apache.flink.runtime.jobmanager.JobGraphStoreFactory;
+import org.apache.flink.runtime.rpc.FatalErrorHandler;
+import org.apache.flink.runtime.rpc.RpcService;
+import org.apache.flink.util.FlinkException;
+import org.apache.flink.util.FlinkRuntimeException;
+
+import java.util.concurrent.Executor;
+
+/**
+ * Factory for the {@link JobDispatcherLeaderProcessFactory}.
+ */
+public class JobDispatcherLeaderProcessFactoryFactory implements DispatcherLeaderProcessFactoryFactory {
+
+	private final JobGraphRetriever jobGraphRetriever;
+
+	private JobDispatcherLeaderProcessFactoryFactory(JobGraphRetriever jobGraphRetriever) {
+		this.jobGraphRetriever = jobGraphRetriever;
+	}
+
+	@Override
+	public DispatcherLeaderProcessFactory createFactory(
+			JobGraphStoreFactory jobGraphStoreFactory,
+			Executor ioExecutor,
+			RpcService rpcService,
+			PartialDispatcherFactoryServices dispatcherFactoryServices,
+			FatalErrorHandler fatalErrorHandler) {
+
+		final JobGraph jobGraph;
+
+		try {
+			jobGraph = jobGraphRetriever.retrieveJobGraph(dispatcherFactoryServices.getConfiguration());
+		} catch (FlinkException e) {
+			throw new FlinkRuntimeException("Could not retrieve the JobGraph.", e);
+		}
+
+		final DispatcherServiceImplFactory dispatcherServiceImplFactory = new DispatcherServiceImplFactory(
+			JobDispatcherFactory.INSTANCE,
+			rpcService,
+			dispatcherFactoryServices);
+
+		return new JobDispatcherLeaderProcessFactory(
+			dispatcherServiceImplFactory,
+			jobGraph,
+			fatalErrorHandler);
+	}
+
+	public static JobDispatcherLeaderProcessFactoryFactory create(JobGraphRetriever jobGraphRetriever) {
+		return new JobDispatcherLeaderProcessFactoryFactory(jobGraphRetriever);
+	}
+}

flink-runtime/src/main/java/org/apache/flink/runtime/dispatcher/runner/SessionDispatcherLeaderProcessFactoryFactory.java
@@ -1,0 +1,62 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.runtime.dispatcher.runner;
+
+import org.apache.flink.runtime.dispatcher.DispatcherFactory;
+import org.apache.flink.runtime.dispatcher.PartialDispatcherFactoryServices;
+import org.apache.flink.runtime.jobmanager.JobGraphStoreFactory;
+import org.apache.flink.runtime.rpc.FatalErrorHandler;
+import org.apache.flink.runtime.rpc.RpcService;
+
+import java.util.concurrent.Executor;
+
+/**
+ * Factory for the {@link DispatcherLeaderProcessImplFactory}.
+ */
+public class SessionDispatcherLeaderProcessFactoryFactory implements DispatcherLeaderProcessFactoryFactory {
+
+	private final DispatcherFactory dispatcherFactory;
+
+	private SessionDispatcherLeaderProcessFactoryFactory(DispatcherFactory dispatcherFactory) {
+		this.dispatcherFactory = dispatcherFactory;
+	}
+
+	@Override
+	public DispatcherLeaderProcessFactory createFactory(
+			JobGraphStoreFactory jobGraphStoreFactory,
+			Executor ioExecutor,
+			RpcService rpcService,
+			PartialDispatcherFactoryServices dispatcherFactoryServices,
+			FatalErrorHandler fatalErrorHandler) {
+		final DispatcherLeaderProcessImpl.DispatcherServiceFactory dispatcherServiceFactory = new DispatcherServiceImplFactory(
+			dispatcherFactory,
+			rpcService,
+			dispatcherFactoryServices);
+
+		return new DispatcherLeaderProcessImplFactory(
+			dispatcherServiceFactory,
+			jobGraphStoreFactory,
+			ioExecutor,
+			fatalErrorHandler);
+	}
+
+	public static SessionDispatcherLeaderProcessFactoryFactory create(DispatcherFactory dispatcherFactory) {
+		return new SessionDispatcherLeaderProcessFactoryFactory(dispatcherFactory);
+	}
+}

tillrohrmann/incubator-flink

flink-runtime/src/main/java/org/apache/flink/runtime/dispatcher/runner/DispatcherLeaderProcessFactoryFactory.java
@@ -1,0 +1,39 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.runtime.dispatcher.runner;
+
+import org.apache.flink.runtime.dispatcher.PartialDispatcherServices;
+import org.apache.flink.runtime.jobmanager.JobGraphStoreFactory;
+import org.apache.flink.runtime.rpc.FatalErrorHandler;
+import org.apache.flink.runtime.rpc.RpcService;
+
+import java.util.concurrent.Executor;
+
+/**
+ * Factory for {@link DispatcherLeaderProcessFactory}.
+ */
+public interface DispatcherLeaderProcessFactoryFactory {
+
+	DispatcherLeaderProcessFactory createFactory(
+		JobGraphStoreFactory jobGraphStoreFactory,
+		Executor ioExecutor,
+		RpcService rpcService,
+		PartialDispatcherServices partialDispatcherServices,
+		FatalErrorHandler fatalErrorHandler);
+}

flink-runtime/src/main/java/org/apache/flink/runtime/dispatcher/runner/JobDispatcherLeaderProcessFactoryFactory.java
@@ -1,0 +1,74 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.runtime.dispatcher.runner;
+
+import org.apache.flink.runtime.dispatcher.JobDispatcherFactory;
+import org.apache.flink.runtime.dispatcher.PartialDispatcherServices;
+import org.apache.flink.runtime.entrypoint.component.JobGraphRetriever;
+import org.apache.flink.runtime.jobgraph.JobGraph;
+import org.apache.flink.runtime.jobmanager.JobGraphStoreFactory;
+import org.apache.flink.runtime.rpc.FatalErrorHandler;
+import org.apache.flink.runtime.rpc.RpcService;
+import org.apache.flink.util.FlinkException;
+import org.apache.flink.util.FlinkRuntimeException;
+
+import java.util.concurrent.Executor;
+
+/**
+ * Factory for the {@link JobDispatcherLeaderProcessFactory}.
+ */
+public class JobDispatcherLeaderProcessFactoryFactory implements DispatcherLeaderProcessFactoryFactory {
+
+	private final JobGraphRetriever jobGraphRetriever;
+
+	private JobDispatcherLeaderProcessFactoryFactory(JobGraphRetriever jobGraphRetriever) {
+		this.jobGraphRetriever = jobGraphRetriever;
+	}
+
+	@Override
+	public DispatcherLeaderProcessFactory createFactory(
+			JobGraphStoreFactory jobGraphStoreFactory,
+			Executor ioExecutor,
+			RpcService rpcService,
+			PartialDispatcherServices partialDispatcherServices,
+			FatalErrorHandler fatalErrorHandler) {
+
+		final JobGraph jobGraph;
+
+		try {
+			jobGraph = jobGraphRetriever.retrieveJobGraph(partialDispatcherServices.getConfiguration());
+		} catch (FlinkException e) {
+			throw new FlinkRuntimeException("Could not retrieve the JobGraph.", e);
+		}
+
+		final DispatcherServiceImplFactory dispatcherServiceImplFactory = new DispatcherServiceImplFactory(
+			JobDispatcherFactory.INSTANCE,
+			rpcService,
+			partialDispatcherServices);
+
+		return new JobDispatcherLeaderProcessFactory(
+			dispatcherServiceImplFactory,
+			jobGraph,
+			fatalErrorHandler);
+	}
+
+	public static JobDispatcherLeaderProcessFactoryFactory create(JobGraphRetriever jobGraphRetriever) {
+		return new JobDispatcherLeaderProcessFactoryFactory(jobGraphRetriever);
+	}
+}

flink-runtime/src/main/java/org/apache/flink/runtime/dispatcher/runner/SessionDispatcherLeaderProcessFactoryFactory.java
@@ -1,0 +1,62 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.runtime.dispatcher.runner;
+
+import org.apache.flink.runtime.dispatcher.DispatcherFactory;
+import org.apache.flink.runtime.dispatcher.PartialDispatcherServices;
+import org.apache.flink.runtime.jobmanager.JobGraphStoreFactory;
+import org.apache.flink.runtime.rpc.FatalErrorHandler;
+import org.apache.flink.runtime.rpc.RpcService;
+
+import java.util.concurrent.Executor;
+
+/**
+ * Factory for the {@link DispatcherLeaderProcessImplFactory}.
+ */
+public class SessionDispatcherLeaderProcessFactoryFactory implements DispatcherLeaderProcessFactoryFactory {
+
+	private final DispatcherFactory dispatcherFactory;
+
+	private SessionDispatcherLeaderProcessFactoryFactory(DispatcherFactory dispatcherFactory) {
+		this.dispatcherFactory = dispatcherFactory;
+	}
+
+	@Override
+	public DispatcherLeaderProcessFactory createFactory(
+			JobGraphStoreFactory jobGraphStoreFactory,
+			Executor ioExecutor,
+			RpcService rpcService,
+			PartialDispatcherServices partialDispatcherServices,
+			FatalErrorHandler fatalErrorHandler) {
+		final DispatcherLeaderProcessImpl.DispatcherServiceFactory dispatcherServiceFactory = new DispatcherServiceImplFactory(
+			dispatcherFactory,
+			rpcService,
+			partialDispatcherServices);
+
+		return new DispatcherLeaderProcessImplFactory(
+			dispatcherServiceFactory,
+			jobGraphStoreFactory,
+			ioExecutor,
+			fatalErrorHandler);
+	}
+
+	public static SessionDispatcherLeaderProcessFactoryFactory create(DispatcherFactory dispatcherFactory) {
+		return new SessionDispatcherLeaderProcessFactoryFactory(dispatcherFactory);
+	}
+}