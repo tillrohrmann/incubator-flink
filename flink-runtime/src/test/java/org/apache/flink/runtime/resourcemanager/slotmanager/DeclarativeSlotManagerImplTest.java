bb2cc47a4467f6fb47399ecc0f12a7be9894be19
Chesnay Schepler <chesnay@apache.org>
2020-08-07 10:00:04 +0200

    Remove unused import

--- flink-runtime/src/test/java/org/apache/flink/runtime/resourcemanager/slotmanager/DeclarativeSlotManagerImplTest.java
+++ flink-runtime/src/test/java/org/apache/flink/runtime/resourcemanager/slotmanager/DeclarativeSlotManagerImplTest.java
@@ -90,7 +90,6 @@ import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 /**
  * Tests for the {@link DeclarativeSlotManagerImpl}.
