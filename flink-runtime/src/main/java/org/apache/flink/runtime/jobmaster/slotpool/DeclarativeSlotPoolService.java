7e1fb92f5467d28dd75aa7202f2b646f5536d786
Till Rohrmann <trohrmann@apache.org>
2020-12-17 14:17:14 +0100

    [FLINK-XXXX] Implement minimal DeclarativeSlotPoolService to accommodate a DeclarativeSlotPool

---
+++ flink-runtime/src/main/java/org/apache/flink/runtime/jobmaster/slotpool/DeclarativeSlotPoolService.java
@@ -0,0 +1,113 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.runtime.jobmaster.slotpool;
+
+import org.apache.flink.api.common.JobID;
+import org.apache.flink.runtime.clusterframework.types.AllocationID;
+import org.apache.flink.runtime.clusterframework.types.ResourceID;
+import org.apache.flink.runtime.concurrent.ComponentMainThreadExecutor;
+import org.apache.flink.runtime.jobmanager.slots.TaskManagerGateway;
+import org.apache.flink.runtime.jobmaster.AllocatedSlotReport;
+import org.apache.flink.runtime.jobmaster.JobMasterId;
+import org.apache.flink.runtime.resourcemanager.ResourceManagerGateway;
+import org.apache.flink.runtime.taskexecutor.slot.SlotOffer;
+import org.apache.flink.runtime.taskmanager.TaskManagerLocation;
+
+import javax.annotation.Nullable;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Optional;
+
+/** {@link SlotPoolService} implementation for the {@link DeclarativeSlotPool}. */
+public class DeclarativeSlotPoolService implements SlotPoolService {
+
+    private final JobID jobId;
+
+    private final DeclarativeSlotPool declarativeSlotPool;
+
+    public DeclarativeSlotPoolService(JobID jobId, DeclarativeSlotPool declarativeSlotPool) {
+        this.jobId = jobId;
+        this.declarativeSlotPool = declarativeSlotPool;
+    }
+
+    @Override
+    public <T> Optional<T> castInto(Class<T> clazz) {
+        if (clazz.isAssignableFrom(declarativeSlotPool.getClass())) {
+            return Optional.of(clazz.cast(declarativeSlotPool));
+        }
+
+        return Optional.empty();
+    }
+
+    @Override
+    public void start(
+            JobMasterId fencingToken,
+            String address,
+            ComponentMainThreadExecutor mainThreadExecutor)
+            throws Exception {}
+
+    @Override
+    public void suspend() {}
+
+    @Override
+    public void close() {}
+
+    @Override
+    public Collection<SlotOffer> offerSlots(
+            TaskManagerLocation taskManagerLocation,
+            TaskManagerGateway taskManagerGateway,
+            Collection<SlotOffer> offers) {
+        return declarativeSlotPool.offerSlots(
+                offers, taskManagerLocation, taskManagerGateway, System.nanoTime());
+    }
+
+    @Override
+    public Optional<ResourceID> failAllocation(
+            @Nullable ResourceID resourceID, AllocationID allocationId, Exception cause) {
+        declarativeSlotPool.releaseSlot(allocationId, cause);
+
+        if (declarativeSlotPool.containsSlots(resourceID)) {
+            return Optional.empty();
+        } else {
+            return Optional.of(resourceID);
+        }
+    }
+
+    @Override
+    public boolean registerTaskManager(ResourceID taskManagerId) {
+        return false;
+    }
+
+    @Override
+    public boolean releaseTaskManager(ResourceID resourceID, Exception cause) {
+        return false;
+    }
+
+    @Override
+    public void connectToResourceManager(ResourceManagerGateway resourceManagerGateway) {}
+
+    @Override
+    public void disconnectResourceManager() {}
+
+    @Override
+    public AllocatedSlotReport createAllocatedSlotReport(ResourceID resourceID) {
+        return new AllocatedSlotReport(jobId, Collections.emptyList());
+    }
+}
